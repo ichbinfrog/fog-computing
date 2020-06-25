779c7bf2ced297b9ad1851a38d8819e38c612594
Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>
2020-06-25 16:21:46 +0200

    add autorun scripts
    Signed-off-by: Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -7,6 +7,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from reader import Reader
 
+def split_cache(x):
+    if "lru" in x:
+        return "lru"
+    elif "lfu" in x:
+        return "lfu"
+    elif "random" in x:
+        return "random"
+    elif "reference" in x:
+        return "reference"
+    
+def split_capacity(x):
+    if "15" in x:
+        return "15"
+    elif "2000" in x:
+        return "2000"
+    
+def split_layers(x):
+    if "3layers" in x:
+        return "3"
+    else:
+        return "4"
+    
+def split_type(x):
+    if "frontier" in x:
+        return "frontier"
+    else:
+        return "everywhere"
 
 class ContentStore(Reader):
     """Implémentation du Reader pour les CS Tracer"""
@@ -23,7 +50,9 @@ class ContentStore(Reader):
         )
         self.data_["NodeType"] = self.data_["Node"].str.replace("\d+", "")
         self.data_["NodeNumber"] = self.data_["Node"].str.replace("[a-zA-Z]+", "")
-
+        self.data_["Cache"] = self.data_["source"].apply((lambda x: split_cache(x)))
+        self.data_["CacheCapacity"] = self.data_["source"].apply((lambda x: split_capacity(x)))
+        self.data_["CacheStrategy"] = self.data_["source"].apply((lambda x: split_type(x)))
 
 class AppDelay(Reader):
     """Implémentation du Reader pour les AppDelay Tracer"""
@@ -36,4 +65,9 @@ class AppDelay(Reader):
         self.data_["Type"] = (
             self.data_["Type"]
             .str.replace("Delay", "")
-        )
+        )        
+        self.data_["NodeType"] = self.data_["Node"].str.replace("\d+", "")
+        self.data_["NodeNumber"] = self.data_["Node"].str.replace("[a-zA-Z]+", "")
+        self.data_["Cache"] = self.data_["source"].apply((lambda x: split_cache(x)))
+        self.data_["CacheCapacity"] = self.data_["source"].apply((lambda x: split_capacity(x)))
+        self.data_["CacheStrategy"] = self.data_["source"].apply((lambda x: split_type(x)))
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+N=8
+(
+for file in $(find scratch/ -mindepth 1 -maxdepth 1); do
+	((i=i%N)); ((i++==0)) && wait
+	sudo ./waf --run=`basename $file` &
+done
+)
