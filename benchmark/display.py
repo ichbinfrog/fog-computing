31ed5b1f4faec0da97e4f0dbc008647d54d74626
Yann Ladeve <yann.ladeve@etu.utc.fr>
2020-06-27 15:22:28 +0200

    update display

--- benchmark/display.py
+++ benchmark/display.py
@@ -8,6 +8,7 @@ import bench
 from string import digits
 from os import listdir
 from os.path import isfile, join, getsize
+from math import *
 
 
 
@@ -19,65 +20,85 @@ def hit_ratio(df):
 	"""Calcul le hit ratio pour chaque noeud du réseau"""
 	hit_ratio = []
 	for i in range(0,df.shape[0],2):
-		if df.iloc[i,3] > 0:
-			hit_ratio.append([df.iloc[i,0],df.iloc[i,1],df.iloc[i,3]/(df.iloc[i,3]+df.iloc[i+1,3])])
-	return pd.DataFrame(data=hit_ratio, columns=['Time','Node','Hit Ratio'])
-	
+		if df.iloc[i+1,3] != 0:
+			hit_ratio.append([df.iloc[i,0],df.iloc[i,7],df.iloc[i,3]/(df.iloc[i,3]+df.iloc[i+1,3])])
+	df = pd.DataFrame(data=hit_ratio, columns=['Time','Cache','Hit Ratio'])
+	return df
 
 
-def display_hit_ratio(df,title):
+def display_hit_ratio(list_df):
 	"""Affiche le hit ratio en fonction du temps pour chaque noeud du réseau"""
-	newDF = hit_ratio(df)
-	if newDF.shape[0] == 0:
-		print(f'{title} is empty')
-	else:
-		sns.lineplot(x='Time',y='Hit Ratio',hue='Node', data=newDF)
-		f = title.replace('.txt','')
-		plt.title(f)
-		plt.legend(loc='lower right', ncol=1, fontsize='xx-small', fancybox=True, shadow=True)
-		plt.show()
+	for pos,i in enumerate(list_df):
+		if i[0].empty == False:
+			label = i[1].replace('.txt','')
+			sns.lineplot(x='Time',y='Hit Ratio', data=i[0], label=label)
+
+	plt.legend(loc='upper right', ncol=1, fontsize='xx-small', fancybox=True, shadow=True)
+	plt.show()
 		#fig.savefig(f'img/{f}.png', dpi=plt.gcf().dpi)
 
 
-#
-#	APP DELAY TRACE
-#
+def display_hit_ratio_cee_15_15s():
+	onlyfiles = ['out/'+f for f in listdir('out/') if isfile(join('out/', f)) and 'cs_grid' in f and 'fog' not in f and 'frontier' not in f]
+	df = bench.ContentStore(onlyfiles).data_
+	df = hit_ratio(df)
+	df['Interval'] = df['Time'] // 2
+	sns.lineplot(x='Interval',y='Hit Ratio', hue='Cache', data=df)
+	sns.set(font_scale=1.1)
+	plt.legend(loc='upper left')
+	plt.figure(figsize=(10,8))
+	plt.show()
 
 
-def delay(df):
-	mean_delay = []
-	df = df.loc[df['Type']=='FullDelay']
-	for i in range(df.shape[0]):
-		mean_delay.append([df.iloc[i,0],df.iloc[i,1],df.iloc[i,5]])
-	return pd.DataFrame(data=mean_delay, columns=['Time','Node', 'Delay'])
+def display_hit_ratio_cee_15_60s():
+	onlyfiles = ['out_back_up/'+f for f in listdir('out_back_up/') if isfile(join('out_back_up/', f)) and 'cs_grid' in f and 'fog' not in f and 'frontier' not in f]
+	df = bench.ContentStore(onlyfiles).data_
+	df = hit_ratio(df)
+	df['Interval'] = df['Time'] // 5
+	sns.lineplot(x='Interval',y='Hit Ratio', hue='Cache', data=df)
+	sns.set(font_scale=1.1)
+	sns.set_style('darkgrid')
+	plt.legend(loc='upper left')
+	plt.figure(figsize=(10,8))
+	plt.show()
 
 
-def display_delay(df,title):
-	newDF = delay(df)
-	if newDF.shape[0] == 0:
-		print(f'{title} is empty')
-	else:
-		sns.lineplot(x='Time',y='Delay', hue='Node', data=newDF)
-		f = title.replace('.txt','')
-		plt.title(f)
-		plt.legend(loc='lower right', ncol=1, fontsize='xx-small', fancybox=True, shadow=True)
-		plt.show()
-		#fig.savefig(f'img/{f}.png',dpi=plt.gcf().dpi)
+def display_hit_ratio_cee_100_15s():
+	onlyfiles = ['out_100/'+f for f in listdir('out_100/') if isfile(join('out_100/', f)) and 'cs_grid' in f and 'fog' not in f and 'frontier' not in f]
+	df = bench.ContentStore(onlyfiles).data_
+	df = hit_ratio(df)
+	df['Interval'] = df['Time'] // 2	
+	sns.lineplot(x='Interval',y='Hit Ratio', hue='Cache', data=df)
+	sns.set(font_scale=1.1)
+	plt.legend(loc='upper left')
+	plt.figure(figsize=(10,8))
+	plt.show()
 
 
-if __name__ == '__main__':
-	onlyfiles = [f for f in listdir('out/') if isfile(join('out/', f))]
-	app_grid = [f for f in onlyfiles if re.search('app_grid',f)]
-	cs_grid = [f for f in onlyfiles if re.search('cs_grid',f)]
+def display_hit_ratio_only_buf():
+	onlyfiles = ['out_back_up/'+f for f in listdir('out_back_up/') if isfile(join('out_back_up/', f)) and 'cs_grid' in f and ('fog' in f or 'reference' in f)]
+	df = bench.ContentStore(onlyfiles).data_
+	df = df.loc[df['NodeType'] == 'buf']
+	df = hit_ratio(df)
+	df['Interval'] = df['Time'] // 2
 	
-	for i in cs_grid:
-		cs = bench.ContentStore([f'out/{i}'])
-		display_hit_ratio(cs.data_,i)
+	sns.set_style("whitegrid")
+	sns.set(font_scale=1.8)
+	sns.lineplot(x='Interval',y='Hit Ratio', hue='Cache', data=df)
+	plt.show()
 
-	for i in app_grid:
-		if getsize(f'out/{i}') != 0:
-			cs = bench.ContentStore([f'out/{i}'])
-			display_delay(cs.data_,i)
+def display_hit_ratio_only_frontier():
+	onlyfiles = ['out_back_up/'+f for f in listdir('out_back_up/') if isfile(join('out_back_up/', f)) and 'cs_grid' in f and ('frontier' in f or 'reference' in f)]
+	df = bench.ContentStore(onlyfiles).data_
+	df = df.loc[df['NodeType'] == 'frt']
+	df = hit_ratio(df)
+	df['Interval'] = df['Time'] // 2
+	
+	sns.set_style("whitegrid")
+	sns.set(font_scale=1.8)
+	sns.lineplot(x='Interval',y='Hit Ratio', hue='Cache', data=df)
+	plt.show()
+
 
 
 
